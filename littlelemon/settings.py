ec88dfac728de1796086494ffd91dd508c787b13
dhruvil-d <dhruvildholakia10@gmail.com>
2026-06-07 23:17:19 +0530

    database changed

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -60,19 +60,10 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'littlelemon.wsgi.application'
 
-# Database – MySQL configuration
-# Update NAME, USER, PASSWORD with your MySQL credentials
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'littlelemon',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
